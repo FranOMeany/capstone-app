a48ce74129f2d9109efaacd298ce90e436adaa05
Francisco J. O'Meany <francisco@omeany.net>
2024-06-01 06:56:42 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_page/Landing_Page';
